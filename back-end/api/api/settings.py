205c91bb4eaa4443efd84a09cfed4e290cdca8aa
Julia Arukakkal <julia.arukakkal@gmail.com>
2025-01-26 01:09:33 -0600

    Add base chatbot to the back-end.

--- back-end/api/api/settings.py
+++ back-end/api/api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'vehicles',
     'rest_framework',
+    'chatbot',
 ]
 
 MIDDLEWARE = [
--- back-end/api/api/urls.py
+++ back-end/api/api/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from chatbot.views import ask_question, chat, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vehicles.urls')),  # Include the vehicles app's routes
+    #path('', include('vehicles.urls')),  # Include the vehicles app's routes
+    path('', home, name="home"),
+    path("ask_question/", ask_question, name="ask_question"),
+    path("chat/", chat, name="chat")
 ]
