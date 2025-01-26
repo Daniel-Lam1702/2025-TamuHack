2765b9b040759ccad1db0249ff836bc3aa33f6b5
bbriannab <bribribb.bb@gmail.com>
2025-01-25 21:10:08 -0600

    home page

--- front-end/src/Vehicles.jsx
+++ front-end/src/Vehicles.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 
 export default function Vehicles() {
-    const[popup, isPopup] = useState(false);
-
     return (
         <div class="">
             <h1>TESTINGGGGG</h1>
